9ca398000524af91b35878193fe423e5b3f33e9d
HrutikBachuwar017 <130389407+HrutikBachuwar017@users.noreply.github.com>
2023-11-06 23:35:46 +0530

    Add files via upload

---
+++ Portfolio/portfolio.js
@@ -0,0 +1,12 @@
+var typed=new Typed(".typing",{
+    strings:["Student","Web developer"],
+    typespeed:100,
+    backspeed:60,
+    loop:true
+})
+var typed=new Typed(".typing-2",{
+    strings:["Student","Web developer"],
+    typespeed:100,
+    backspeed:60,
+    loop:true
+})
